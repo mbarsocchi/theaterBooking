5273c1e5f8fefd51d359f45ba2dd7c13adb26e5a
Miro Barsocchi <mbarsocchi@soldo.com>
2023-12-14 22:34:25 +0100

    fix mobile view

--- view/part_meta_head.php
+++ view/part_meta_head.php
@@ -1,3 +1,4 @@
+<?php echo $version="1.0"; ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Gestisci le prenotazioni</title>
         <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-        <link rel="stylesheet" href="css/theater_style.css?v=1.1">
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <link rel="stylesheet" href="css/theater_style.css?v=<?php echo $version; ?>">
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js?v=<?php echo $version; ?>"></script>
         <script src="js/ajax_edittable.js"></script>
         <script src="js/func.js"></script>
     </head>
