83abdf94769dc05a4485f29b05822460b9f9f3ce
mbarsocchi <mbarsocchi@soldo.com>
2026-06-09 15:59:24 +0200

    added icon

--- view/part_meta_head.php
+++ view/part_meta_head.php
@@ -5,7 +5,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Gestisci le prenotazioni</title>
-        <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+        <link rel="icon" type="image/png" href="img/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="img/favicon.svg" />
+        <link rel="shortcut icon" href="img/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Prenota" />
+        <link rel="manifest" href="img/site.webmanifest" />
         <link rel="stylesheet" href="css/theater_style.css?v=<?php echo $version; ?>">
         <script src="https://code.jquery.com/jquery-3.7.1.min.js?v=<?php echo $version; ?>"></script>
         <script src="js/ajax_edittable.js"></script>
